e8e05d3b9cfccba1a8f722511d47145b193b550c
hhh <jareen0512@gmail.com>
2025-11-01 16:43:15 +0600

    RPS2

--- main.py
+++ main.py
@@ -26,6 +26,10 @@ rock_image = pygame.image.load("Rock.png")
 paper_image = pygame.image.load("Paper.png")
 scissor_img = pygame.image.load("Scissor.png")
 
+#Resize all images to fit buttons nicely
+rock_image = pygame.transform.scale(rock_image, (100, 100))
+paper_image = pygame.transform.scale(paper_image, (100, 100))
+scissor_img = pygame.transform.scale(scissor_img, (100, 100))
 #Choises and their images
 choices = {
     "Rock": rock_image,
@@ -56,3 +60,56 @@ def decide_winner(player, computer):
         return "You Win!"
     else:
         return "You Lose!"
+#--MAIN GAME LOOP---
+running = True
+while running:
+    screen.fill(WHITE)
+
+    #---TITLE---
+    title_text = big_font.render("Rock Paper Scissors", True, BLUE)
+    screen.blit(title_text, (WIDTH//2 - title_text.get_width()//2, 20))
+
+    #---SHOW INSTUCTIONS---
+    info_text = font.render("Choose your move:", True, BLACK)
+    screen.blit(info_text, (WIDTH//2 - info_text.get_width()//2, 60))
+
+    #---DISPLAY PLAYER AND COMPUTER CHOISES---
+    if player_choise:
+        #player side
+        screen.blit(choices[player_choise], (120, 150))
+        player_label = font.render("You", True, BLACK)
+        screen.blit(player_label, (160, 250))
+    if computer_choise:
+        #Computer side
+        screen.blit(choices[computer_choise], (380, 150))
+        comp_label = font.render("Computer", True, BLACK)
+        screen.blit(comp_label, (380, 250))
+
+    #---DISPLAY RESULT---
+    if result:
+        result_text = big_font.render(result, True, BLUE)
+        screen.blit(result_text, (WIDTH//2 - result_text.get_width()//2, 100))
+    #---DRAW BUTTONS(IMAGES AS BUTTONS)---
+    for name, rect in buttons.items():
+        #Drawa light gray background for the button
+        pygame.draw.rect(screen, GRAY, rect, border_radius=12)
+        #Draw images on top
+        screen.blit(choices[name], (rect.x, rect.y))
+
+    #---EVENT HANDLING---
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+            sys.exit
+        
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            #Check if any button was clicked
+            for name, rect in buttons.items():
+                if rect.collidepoint(event.pos):
+                    player_choise = name
+                    computer_choise = random.choice(list(choices.keys()))
+                    result = decide_winner(player_choise, computer_choise)
+
+    #---UPDATE DISPLAY---
+    pygame.display.flip()
